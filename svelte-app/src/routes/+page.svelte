17fbfbc149f871489e58481c9d4735373c3931f6
JakePaulKelly <148543950+JakePaulKelly@users.noreply.github.com>
2025-04-11 00:30:39 +0100

    Logo and Moving clouds

--- svelte-app/src/routes/+page.svelte
+++ svelte-app/src/routes/+page.svelte
@@ -1,2 +1,109 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<div class="overlay">
+    <div class="cloud cloud-1"></div>
+    <div class="cloud cloud-2"></div>
+    <div class="cloud cloud-3"></div>
+</div>
+<div class="jake_persona">
+    <div class="reveal">
+        <img src="/sketch logo-w-g-2.png" alt="Jake Persona" style="width: 350px" class="flex-image">
+    </div>
+</div>
+
+<style>
+    .overlay {
+        position: fixed;    
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: linear-gradient(to top, 
+            rgb(25, 125, 255) 0%,
+            rgb(25, 159, 255) 50%,
+            rgb(0, 38, 144)) 100%;
+        z-index: -1;
+        overflow: hidden;
+    }
+
+    h1{
+        text-align: center;
+        font-family: "Lexend Giga", sans-serif;
+        font-size: 0rem;
+    }
+
+    .cloud {
+        position:fixed  ;
+        background-image: url('/cloud.png');
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: center;
+        pointer-events: none;
+    }
+
+    .cloud-1 {
+        width: 600px;
+        height: 350px;
+        top: 10%;
+        animation: moveCloud 60s linear infinite;
+        transform: scaleX(-1);
+    }
+
+    .cloud-2 {
+        width: 600px;
+        height: 500px;
+        top: 30%;
+        left: -450px;   
+        animation: moveCloud 40s linear infinite;
+        z-index: 2;
+    }
+
+    .cloud-3 {
+        width: 600px;
+        height: 350px;
+        top: 50%;
+        left: -500px;
+        opacity:0.5;
+        animation: moveCloud 60s linear infinite;
+    }
+
+    
+    .jake_persona {
+        max-width: 120px;
+        margin: 0 auto;
+        height: 65vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding-top: 1rem;
+    }
+
+    .reveal {
+        opacity: 0;
+        animation: revealPersona 1s forwards ease-out;
+    }
+
+    @keyframes moveCloud {
+        0% {
+            left: -600px;
+        }
+        100% {
+            left: 100%;
+        }
+    }
+    
+    @keyframes revealPersona {
+        0% {
+            opacity: 0;
+            transform: translateY(20px);
+            scale: 1.15;
+        }
+        100% {
+            opacity: 1;
+            transform: translateY(0);
+            scale: 1.25;
+        }
+    }
+
+</style>
+
